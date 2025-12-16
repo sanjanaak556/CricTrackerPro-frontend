521cd040f244b5257d51493e4e0328af34c94337
Sanjana A K <sanjanaak556@gmail.com>
2025-12-16 17:41:43 +0530

    updated socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";   // backend URL
+// Socket URL from env (Render / local)
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 export const socket = io(SOCKET_URL, {
-  transports: ["websocket"], 
+  transports: ["websocket"],
   withCredentials: true,
 });
